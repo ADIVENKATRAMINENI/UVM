272e86118a82c2b71436f222bf72b0421aaa1a21
ADI VENKAT RAMINENI <113154624+ADIVENKATRAMINENI@users.noreply.github.com>
2025-11-16 23:20:43 -0500

    Update seq_item.sv

--- Projects/UART_Verification/UVM/seq_item/seq_item.sv
+++ Projects/UART_Verification/UVM/seq_item/seq_item.sv
@@ -1,1 +1,25 @@
-s
+`timescale 1ns/1ps
+ import uvm_pkg::*; 
+`include "uvm_macros.svh"
+
+
+// Transaction_item
+
+class uart_txn extends uvm_sequence_item;
+  rand bit[7:0] data;
+  rand int unsigned idle_cycles;
+  
+  constraint idle_c{idle_cycles inside {[0:10]};}
+  
+  `uvm_object_utils(uart_txn)
+  
+  function new(string name="uart_txn");
+    super.new(name);
+  endfunction
+  
+  
+  function string convert2string();
+    return $sformatf("{data =0x%0h idle=%0d}",data,idle_cycles);
+  endfunction
+  
+endclass
