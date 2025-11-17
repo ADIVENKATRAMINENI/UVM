fdbbb0ceb1e1eb4ae8a29991f548bd6d57ca3a9f
ADI VENKAT RAMINENI <113154624+ADIVENKATRAMINENI@users.noreply.github.com>
2025-11-16 23:27:37 -0500

    Update seqr.sv

--- Projects/UART_Verification/UVM/seqr/seqr.sv
+++ Projects/UART_Verification/UVM/seqr/seqr.sv
@@ -1,1 +1,11 @@
-f
+
+// sequencer
+
+class uart_seqr extends uvm_sequencer#(uart_txn);
+  `uvm_component_utils(uart_seqr)
+  
+  function new(string name,uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+endclass
