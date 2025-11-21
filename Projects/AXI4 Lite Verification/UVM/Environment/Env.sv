fc24715dc577f384d7b68d561081134b1323c657
ADI VENKAT RAMINENI <113154624+ADIVENKATRAMINENI@users.noreply.github.com>
2025-11-21 12:01:10 -0500

    Add axi_env class for AXI4 Lite verification

--- Projects/AXI4 Lite Verification/UVM/Environment/Env.sv
+++ Projects/AXI4 Lite Verification/UVM/Environment/Env.sv
@@ -1,1 +1,22 @@
+class axi_env extends uvm_env;
+  `uvm_component_utils(axi_env)
+  axi_agent agt;
+  axi_scoreboard scb;
+  axi_coverage  cov;
+
+  function new(string name, uvm_component parent); super.new(name,parent); endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    agt = axi_agent::type_id::create("agt",this);
+    scb = axi_scoreboard::type_id::create("scb",this);
+    cov = axi_coverage::type_id::create("cov",this);
+  endfunction
+
+  function void connect_phase(uvm_phase phase);
+    super.connect_phase(phase);
+    agt.mon.ap_wr.connect(scb.imp_wr);
+    agt.mon.ap_rd.connect(scb.imp_rd);
+  endfunction
+endclass
 
