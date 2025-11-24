277f3ea069c8a9e42acf2b2e2c5874b244c20e10
ADI VENKAT RAMINENI <113154624+ADIVENKATRAMINENI@users.noreply.github.com>
2025-11-23 23:13:00 -0500

    Update cfg_if.sv

--- Projects/AXI4 + DMA Subsystem/DUT/Configuration Interface/cfg_if.sv
+++ Projects/AXI4 + DMA Subsystem/DUT/Configuration Interface/cfg_if.sv
@@ -1,1 +1,34 @@
+interface cfg_if(input logic clk, input logic reset);
+    logic [31:0] src_addr;
+    logic [31:0] dst_addr;
+    logic [31:0] length;
+    logic start;
+    logic done;
+
+    // reset logic
+    always_ff @(posedge clk or posedge reset) begin
+        if(reset) begin
+            src_addr <= 0;
+            dst_addr <= 0;
+            length   <= 0;
+            start    <= 0;
+            done     <= 0;
+        end
+    end
+
+    // Clocking for driver
+    clocking drv_cb @(posedge clk);
+        output src_addr, dst_addr, length, start;
+        input  done;
+    endclocking
+
+    // Clocking for monitor
+    clocking mon_cb @(posedge clk);
+        input src_addr, dst_addr, length, start, done;
+    endclocking
+
+    // Modports
+    modport drv (clk, reset, cb => drv_cb);
+    modport mon (clk, reset, cb => mon_cb);
+endinterface
 
