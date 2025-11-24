704f4606fbe52d43490f563921e83b9baf8afc1b
ADI VENKAT RAMINENI <113154624+ADIVENKATRAMINENI@users.noreply.github.com>
2025-11-23 23:50:23 -0500

    Update Seqr.sv

--- Projects/AXI4 + DMA Subsystem/UVM/Sequencer/Seqr.sv
+++ Projects/AXI4 + DMA Subsystem/UVM/Sequencer/Seqr.sv
@@ -1,8 +1,11 @@
-class dma_seqr extends uvm_sequencer#(dma_txn);
-  `uvm_component_utils(dma_seqr)
-  
-  function void(string name, uvm_component parent);
-    super.new(name,parent);
-  endfunction
-  
+// dma_sequencer.sv
+`include "dma_pkg.sv"
+
+class dma_sequencer extends uvm_sequencer #(dma_txn);
+    `uvm_component_utils(dma_sequencer)
+
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction
+
 endclass
