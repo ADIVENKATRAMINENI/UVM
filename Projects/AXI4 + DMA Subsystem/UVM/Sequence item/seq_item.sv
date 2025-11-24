f50c35adefc76ddc08dcb0cdc3b21fb7ef6162cc
ADI VENKAT RAMINENI <113154624+ADIVENKATRAMINENI@users.noreply.github.com>
2025-11-23 23:13:54 -0500

    Create dma_txn class in seq_item.sv
    Added dma_txn class for DMA transaction modeling.

--- Projects/AXI4 + DMA Subsystem/UVM/Sequence item/seq_item.sv
+++ Projects/AXI4 + DMA Subsystem/UVM/Sequence item/seq_item.sv
@@ -1,1 +1,17 @@
+package dma_pkg;
+    import uvm_pkg::*;
+    `include "uvm_macros.svh"
+
+    class dma_txn extends uvm_sequence_item;
+        rand bit [31:0] src_addr;
+        rand bit [31:0] dst_addr;
+        rand bit [31:0] length;
+
+        `uvm_object_utils(dma_txn)
+
+        function new(string name="dma_txn");
+            super.new(name);
+        endfunction
+    endclass
+endpackage
 
