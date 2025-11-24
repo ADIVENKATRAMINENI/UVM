b717748ce45ffead8010b7114c484506fbef47dc
ADI VENKAT RAMINENI <113154624+ADIVENKATRAMINENI@users.noreply.github.com>
2025-11-23 23:14:12 -0500

    Add dma_seqr class extending uvm_sequencer

--- Projects/AXI4 + DMA Subsystem/UVM/Sequencer/Seqr.sv
+++ Projects/AXI4 + DMA Subsystem/UVM/Sequencer/Seqr.sv
@@ -1,1 +1,8 @@
-
+class dma_seqr extends uvm_sequencer#(dma_txn);
+  `uvm_component_utils(dma_seqr)
+  
+  function void(string name, uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  
+endclass
